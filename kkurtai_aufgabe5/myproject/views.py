80918f3973f92f3da610864e726211dcc5a0a076
KrisztinaKurtai <97094321+KrisztinaKurtai@users.noreply.github.com>
2024-01-22 20:19:43 +0100

    Delete kkurtai_aufgabe5 directory

--- kkurtai_aufgabe5/myproject/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, "hello/home.html")
-def about(request):
-    return render(request, "hello/about.html")
-def contact(request):
-    return render(request, "hello/contact.html")
--- kkurtai_aufgabe5/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='user_home'),
-    path('', views.index, name='user_about'),
-    path('', views.index, name='user_contact'),
-]
